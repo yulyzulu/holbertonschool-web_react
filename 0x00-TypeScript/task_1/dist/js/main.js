78a4d4fb50081c8c8baa0a54f996a9491e39b585
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-06-22 18:52:28 -0500

    Printing teachers

--- 0x00-TypeScript/task_1/dist/js/main.js
+++ 0x00-TypeScript/task_1/dist/js/main.js
@@ -2,5 +2,4 @@ var printTeacher;
 printTeacher = function (firstName, lastName) {
     return firstName.charAt(0) + ". " + lastName;
 };
-console.log(printTeacher("John", "Doe"));
 //# sourceMappingURL=main.js.map
