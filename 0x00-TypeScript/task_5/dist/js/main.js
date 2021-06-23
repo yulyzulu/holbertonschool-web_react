33841f85a04c757c857737938b73f23a15c7c56c
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-06-22 23:08:35 -0500

    Credits

--- 0x00-TypeScript/task_5/dist/js/main.js
+++ 0x00-TypeScript/task_5/dist/js/main.js
@@ -1,8 +1,8 @@
-function sumMajorCredits(subject1, subject2) {
+export function sumMajorCredits(subject1, subject2) {
     var sum = subject1.credits + subject2.credits;
     return { credits: sum };
 }
-function sumMinorCredits(subject1, subject2) {
+export function sumMinorCredits(subject1, subject2) {
     var sum = subject1.credits + subject2.credits;
     return { credits: sum };
 }
--- 0x00-TypeScript/task_5/js/main.ts
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -1,18 +1,19 @@
-interface MajorCredits {
+export interface MajorCredits {
     credits: number;
     _mayor: void;
 }
 
-interface MinorCredits {
+export interface MinorCredits {
     credits: number;
     _minor: void;
 }
 
-function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
   const sum = subject1.credits + subject2.credits;
   return { credits: sum } as MajorCredits;
 }
-function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+
+export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
   const sum = subject1.credits + subject2.credits;
   return { credits: sum } as MinorCredits;
 }
