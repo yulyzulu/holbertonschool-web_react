ab5899ea55370e8bfe374544bc95399c6c57e1f6
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-06-22 21:34:44 -0500

    Teacher

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -1,10 +1,10 @@
 interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [propName: string]: any;
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
 }
 
 // const teacher3: Teacher = {
@@ -18,7 +18,7 @@ interface Teacher {
 // console.log(teacher3);
 
 interface Directors extends Teacher {
-    numberOfReports: number;
+  numberOfReports: number;
 }
 
 // const director1: Directors = {
@@ -31,40 +31,40 @@ interface Directors extends Teacher {
 //   console.log(director1);
 
 interface printTeacherFunction {
-    (firstName: string, lastName: string): string;
+  (firstName: string, lastName: string): string;
 }
 
 export const printTeacher: printTeacherFunction = function(firstName: string, lastName: string): string {
-    return `${firstName.charAt(0)}. ${lastName}`;
+  return `${firstName.charAt(0)}. ${lastName}`;
 };
 
 console.log(printTeacher("John", "Doe"));
 
-interface studentClassInterface {
-    firstName: string;
-    lastName: string;
-    workOnHomework(): string;
-    displayName(): string;
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
 }
 
-interface studentConstructor {
-    new(firstName: string, lastName: string): studentClassInterface;
+interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
 }
 
-export const StudentClass: studentConstructor = class StudentClass implements studentClassInterface {
-    firstName: string;
-    lastName: string;
+export const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
 
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+  constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+  }
 
-    workOnHomework(): string {
-        return 'Currently working';
-    }
+  workOnHomework(): string {
+    return 'Currently working';
+  }
 
-    displayName(): string {
-        return this.firstName;
-    }
+  displayName(): string {
+    return this.firstName;
+  }
 }
