2cff26123bd9332b061dc8f0ee5742619501a8cc
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-06-22 22:29:32 -0500

    Ambient Namespaces

---
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -0,0 +1,14 @@
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud";
+
+const row: RowElement = {
+    firstName: 'Guillaume',
+    lastName: 'Salva'
+}
+
+const newRowID: RowID = CRUD.insertRow(row);
+const updateRow: RowElement = { ...row, age: 23 };
+
+CRUD.updateRow(newRowID, updateRow);
+CRUD.deleteRow(newRowID);
+
