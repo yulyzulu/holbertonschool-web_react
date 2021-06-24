19043d9c9c56b83bcdd3c8b4669902f1969f7c02
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-06-24 15:52:36 -0500

    Webpack

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+var $ = require( "jquery" );
+
+$( "p" ).add( "Holberton Dashboard" ).appendTo( document.body );
+$( "p" ).add( "Dashboard data for the students" ).appendTo( document.body );
+$( "p" ).add( "Copyright - Holberton School" ).appendTo( document.body );
