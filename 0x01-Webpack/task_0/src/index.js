4d5b5a9dca31d5d0c392c5190a70d9c190dc5819
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-06-24 16:23:48 -0500

    Webpack

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,5 +1,10 @@
 const $ = require( "jquery" );
 
-$('<p>').text( "Holberton Dashboard" ).appendTo('body');
-$('<p>').text( "Dashboard data for the students" ).appendTo('body');
-$('<p>').text( "Copyright - Holberton School" ).appendTo('body');
+// $('<p>').text( "Holberton Dashboard" ).appendTo('body');
+// $('<p>').text( "Dashboard data for the students" ).appendTo('body');
+// $('<p>').text( "Copyright - Holberton School" ).appendTo('body');
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
