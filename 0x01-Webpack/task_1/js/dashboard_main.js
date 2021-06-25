2a778fa304b5d9881b8b4da5c32bfd507f907736
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-06-24 19:01:41 -0500

    Webpack config

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,5 @@
-const $ = require('jquery');
-const _ = require('lodash');
+import $ from 'jquery';
+import _ from'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
