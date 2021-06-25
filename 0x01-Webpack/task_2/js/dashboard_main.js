44a945b5a2473f3fa98c4759292e506c200b5059
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-06-24 22:13:57 -0500

    Adding CSS and Images

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,18 @@
+const $ = require('jquery');
+const _ = require('lodash');
+import '../css/main.css';
+
+$('body').append('<div id="logo"></div>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+const updateCounter = () => {
+  count = count + 1;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$('button').on('click', _.debounce(updateCounter, 500));
---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/dashboard_main.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                use: ['file-loader', 'image-webpack-loader'],
+            },
+        ]
+    }
+};
