12dc9f32c9e459e21d092ecce23b89702322962f
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-07-18 09:04:48 -0500

    gh-pages

---
+++ 0x02-react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div><hr />
+      <body>
+        <div className="App-body">
+          <p>Login to access the full dashboard</p>
+          <label htmlFor="email" >Email:</label>
+          <input id="email" type="email" />
+          <label htmlFor="password" >Password:</label>
+          <input id="password" type="password" />
+          <button>OK</button>
+        </div><hr />
+        <div className="App-footer">
+          <i>
+            Copyright {getFullYear()} - {getFooterCopy()}
+            </i>
+          </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
