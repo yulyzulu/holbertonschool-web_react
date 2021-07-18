301fdc65f2b0e37c51117825b427428abec19a91
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-07-18 13:32:48 -0500

    task5

---
+++ 0x02-react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,18 @@
+import Notifications from './Notifications';
+import { shallow } from 'enzyme';
+
+describe('<Notifications /> test', () => {
+  it('Should render without crashing', () => {
+      const wrapper = shallow(<Notifications />);
+      expect(wrapper.exists()).toBeTruthy();
+  });
+  it('Should render three list items', () => {
+    const wrapper = shallow(<Notifications />);
+    expect(wrapper.find('li')).toHaveLength(3);
+  });
+  it('Should render the text => Here is the list of notifications', () => {
+      const wrapper = shallow(<Notifications />);
+      const msg = 'Here is the list of notifications'
+    expect(wrapper.find('.Notifications p').text()).toEqual(msg);
+  })
+});
