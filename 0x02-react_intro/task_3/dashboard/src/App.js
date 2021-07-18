60ee84c1ca52d312e140c6785ef67a615f29e598
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-07-18 08:22:35 -0500

    Test

--- 0x02-react_intro/task_3/dashboard/src/App.js
+++ 0x02-react_intro/task_3/dashboard/src/App.js
@@ -5,24 +5,24 @@ import { getFullYear, getFooterCopy } from './utils';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
-      </header><hr />
+      </div><hr />
       <body>
-        <main className="App-body">
+        <div className="App-body">
           <p>Login to access the full dashboard</p>
           <label htmlFor="email" >Email:</label>
           <input id="email" type="email" />
           <label htmlFor="password" >Password:</label>
           <input id="password" type="password" />
           <button>OK</button>
-        </main><hr />
-        <footer className="App-footer">
+        </div><hr />
+        <div className="App-footer">
           <i>
             Copyright {getFullYear()} - {getFooterCopy()}
             </i>
-          </footer>
+          </div>
       </body>
     </div>
   );
