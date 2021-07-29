d6027bd40229c5228ffeb7843727310090eaae0a
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-07-28 23:52:08 -0500

    Chained Mutations

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -1,12 +1,12 @@
 import { Map, List } from 'immutable';
 
-export const concatElements = (page1, page2) => {
+export function concatElements(page1, page2) {
   const page1 = List(page1);
   const page2 = List(page2);
   return page1.concat(page2);
 };
 
-export const mergeElements = (page1, page2) => {
+export function mergeElements(page1, page2) {
   const page1 = Map(page1);
   const page2 = Map(page2);
   return page1.merge(page2);
