4767f6bd35828651681a8e229dabbc096e1fc46c
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-07-29 00:37:21 -0500

    Lazy seq

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -2,7 +2,8 @@ import { seq } from 'immutable';
 
 export default function printBestStudents(obj) {
   const filterByScore = seq(obj).filter((student) => student.score >= 70)
-    .map((student) => {
+    .map((studentItem) => {
+      const student = studentItem;
       student.firstName = student.firstName[0].toUpperCase() + student.firstName.slice(1);
       student.lastName = student.lastName[0].toUpperCase + student.lastName.slice(1);
       return student.toJS;
