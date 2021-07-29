28e63a0b4f5f6fd0ba84a1cbc4ef8406a3e4dd03
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-07-29 00:49:29 -0500

    Mutations

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-export const map = Map( {
+export const map = Map({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
@@ -9,6 +9,6 @@ export const map = Map( {
   6: 'Lucas',
 });
 
-export const map2 = map.withMutations(function(item) {
-  item.set(2, Benjamin).set(4, 'Oliver');
+export const map2 = map.withMutations((item) => {
+  item.set(2, 'Benjamin').set(4, 'Oliver');
 });
