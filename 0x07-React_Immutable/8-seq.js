122aa39361eb79fa0d11121e8daf02d95518cf50
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-07-29 00:46:28 -0500

    Lazy seq

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -7,6 +7,6 @@ export default function printBestStudents(obj) {
       student.firstName = student.firstName[0].toUpperCase() + student.firstName.slice(1);
       student.lastName = student.lastName[0].toUpperCase() + student.lastName.slice(1);
       return student;
-    }).toObject;
+    }).toObject();
   console.log(filterByScore);
 }
