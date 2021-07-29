9507275d77b00ba672cdf3f92a36febfa2afa484
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-07-28 23:50:30 -0500

    Merge & concat

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -1,12 +1,12 @@
 import { Map, List } from 'immutable';
 
-const concatElements = (page1, page2) => {
+export const concatElements = (page1, page2) => {
   const page1 = List(page1);
   const page2 = List(page2);
   return page1.concat(page2);
 };
 
-const mergeElements = (page1, page2) => {
+export const mergeElements = (page1, page2) => {
   const page1 = Map(page1);
   const page2 = Map(page2);
   return page1.merge(page2);
