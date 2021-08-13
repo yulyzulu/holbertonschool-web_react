8472815850e31712d255fb3fd0a7f287376a2a2f
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-08-13 16:39:39 -0500

    Lifecycles

---
+++ 0x04-React_component/task_1/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import NotificationItem from "./NotificationItem";
+
+import { shallow } from 'enzyme';
+
+describe('<NotificationItem /> test', () => {
+  it('Should render without crashing', () => {
+      const wrapper = shallow(<NotificationItem />);
+      expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it('Should render correct html by passing dummy type and value props', () => {
+    const wrapper = shallow(<NotificationItem type="default" value="test" />);
+    expect(wrapper.find('li').text()).toEqual('test');
+    expect(wrapper.find('li').prop('data-notification-type')).toEqual('default');
+  });
+
+  it('Should render correct html by passing a dummy html prop', () => {
+    const wrapper = shallow(<NotificationItem html={{ __html: '<u>test</u>' }} />);
+    expect(wrapper.html()).toEqual('<li data-notification-type="default"><u>test</u></li>');
+  });
+});
